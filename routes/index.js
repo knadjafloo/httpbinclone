92d6ff0f5673a216f362dfa356a7f5ab0471974d
Kamyar Nadjafloo <knadjafloo@gmail.com>
2015-06-02 22:47:52 -0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -23,14 +23,33 @@ router.get('/ip', function(req, res, next) {
 });
 
 router.get('/user-agent', function(req, res, next) {
-	var source = req.headers['user-agent'];
- 	res.json( {"user-agent" : source });
+	var ua = {};
+	ua['user-agent'] = req.headers['user-agent'];
+ 	prettyJson(res, ua);
 });
-
 router.get('/headers', function(req, res, next) {
 	var headers = {};
 	headers['headers'] = req.headers;
-	res.setHeader('Content-Type', 'application/json');
- 	res.send(JSON.stringify(headers, null, 2));
+	prettyJson(res, headers);
 });
+router.post('/post', function(req, res) {
+	var obj = {};
+	obj['form'] = req.body;
+	obj['args'] = req.query;
+	obj['headers'] = req.headers;
+
+	prettyJson(res, obj);
+});
+router.put('/put', function(req, res) {
+	prettyJson(res, req.body);
+});
+router.patch('/patch', function(req, res) {
+	prettyJson(res, req.body);
+});
+
+prettyJson = function(res, data) {
+	res.setHeader('Content-Type', 'application/json');
+  	res.send(JSON.stringify(data, null, 2));
+};
+
 module.exports = router;
